19e0170f40552b3caf68c0e647e96115b4ae31f3
hxkhxk <2498850578@qq.com>
2023-01-31 19:42:03 +0800

    提交消息“（只是在本地提交，还没有提交到Github上） git remote add origin http://github.com/jthhxk/forum.git
    git push origin master
    git commit -m 提交消息
    git commit -m 提交消息“（只是在本地提交，还没有提交到Github上）

---
+++ src/test/java/cn/com/forum/lab25/Lab25ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.forum.lab25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
